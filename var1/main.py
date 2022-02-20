1efbcf05d35f4c958d90a40577e041a74a6d69ce
simon <sajmoooonsk@gmail.com>
2022-02-20 17:37:49 +0100

    feat: add init script

---
+++ var1/main.py
@@ -0,0 +1,26 @@
+from collections import Counter
+from fei.ppds import Thread
+
+
+class Shared:
+    def __init__(self, size):
+        self.counter = 0
+        self.end = size
+        self.elms = [0] * size
+
+
+def do_count(shared):
+    while True:
+        if shared.counter >= shared.end:
+            break
+        shared.elms[shared.counter] += 1
+
+
+shared = Shared(1_000_000)
+t1 = Thread(do_count, shared)
+t2 = Thread(do_count, shared)
+t1.join()
+t2.join()
+
+counter = Counter(shared.elms)
+print(counter.most_common())
