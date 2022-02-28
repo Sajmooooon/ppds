c774c275c0a606e44e21d131771d50df38eb075f
simon <sajmoooonsk@gmail.com>
2022-02-28 20:30:33 +0100

    docs: add docstrings for adt simple barrier

--- simpleBarrier/barrier.py
+++ simpleBarrier/barrier.py
@@ -1,16 +1,35 @@
+"""
+Authors: Bc. Simon Youssef
+         Mgr. Ing. Matúš Jókay, PhD.
+Coppyright 2022 All Rights Reserved.
+
+Basic example of synchronization pattern Semaphore.
+"""
+
 from random import randint
 from time import sleep
 from fei.ppds import Thread, Semaphore, Mutex, print
 
 
 class SimpleBarrier:
+    """The SimpleBarrier class."""
+
     def __init__(self, count):
+        """
+        The constructor for SimpleBarrier class.
+
+        Parameter:
+            count (int): The number of threads.
+        """
+
         self.count = count
         self.counter = 0
         self.mutex = Mutex()
         self.semaphore = Semaphore(0)
 
     def wait(self):
+        """"The SimpleBarrier function with Semaphore."""
+
         self.mutex.lock()
         self.counter += 1
         if self.counter == self.count:
@@ -21,6 +40,14 @@ class SimpleBarrier:
 
 
 def barrier_cycle(barrier, thread_id):
+    """"
+    The function prints threads id before and after barrier.
+
+    Parameters:
+        barrier (obj): The SimpleBarrier object.
+        thread_id (int): The id of thread.
+    """
+
     sleep(randint(1, 10) / 10)
     print(f"before barrier {thread_id}")
     barrier.wait()
