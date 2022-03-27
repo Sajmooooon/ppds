0053ee6fd4d75c03738cf7f5f7e94865255ad654
simon <sajmoooonsk@gmail.com>
2022-03-27 15:09:09 +0200

    docs: add docstrings

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
+"""
+Authors: Bc. Simon Youssef
+         Mgr. Ing. Matúš Jókay, PhD.
+Coppyright 2022 All Rights Reserved.
+Implementation of the problem of barber and multiple customers.
+"""
+
+
 from time import sleep
 from random import randint
 from fei.ppds import Thread, print, Mutex, Semaphore
 
 
 class Shared(object):
+    """This is a shared class for multiple threads."""
+
     def __init__(self, number):
+        """
+        The constructor for Shared class.
+
+        Parameter:
+        number (int): The capacity of barber shop.
+        """
+
         self.N = number
         self.customers = 0
         self.mutex = Mutex()
@@ -16,27 +33,58 @@ class Shared(object):
 
 
 def get_hair_cut(customer_id):
+    """"
+    The simple function to simulate getting haircut.
+
+    Parameter:
+        customer_id (int): The ID of customer.
+    """
+
     print(f"Customer {customer_id}: Getting my hair haircut.")
     sleep(1/10)
 
 
 def cut_hair():
+    """"The simple function to simulate cutting hair."""
+
     print(f"\nBarber: Cutting hair.")
     sleep(1/10)
 
 
 def balk(customer_id):
+    """"
+    The simple function to simulate leaving from barber shop because is full.
+
+    Parameter:
+        customer_id (int): The ID of customer.
+    """
+
     print(f"Customer {customer_id}: The barber shop is full, "
           f"I will come next time.")
     sleep(randint(2, 3)/10)
 
 
 def groving_hair(customer_id):
+    """"
+    The simple function to simulate waiting for hair growth.
+
+    Parameter:
+        customer_id (int): The ID of customer.
+    """
+
     print(f"Customer {customer_id}: waiting for my hair to grow.")
     sleep(randint(3, 5)/10)
 
 
 def customer(customer_id, shared):
+    """
+    The function simulates the customer at the barber shop.
+
+    Parameters:
+        customer_id (int): The ID of customer.
+        shared (object): The shared object.
+    """
+
     while True:
         shared.mutex.lock()
         if shared.customers == shared.N:
@@ -61,6 +109,13 @@ def customer(customer_id, shared):
 
 
 def barber(shared):
+    """
+    The function simulates hair cutting by barber.
+
+    Parameters:
+        shared (object): The shared object.
+    """
+
     while True:
         shared.customer.wait()
         shared.barber.signal()
@@ -70,6 +125,8 @@ def barber(shared):
 
 
 def main():
+    """This function is for program initialization."""
+
     capacity = 2
     customers = 3
     shared = Shared(capacity)
