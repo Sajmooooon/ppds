aeb10e3edc09e833b4aa4077013545f1c1f3078a
simon <sajmoooonsk@gmail.com>
2022-04-09 13:10:37 +0200

    docs: add docstrings

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+"""
+Authors: Bc. Simon Youssef
+         Mgr. Ing. Matúš Jókay, PhD.
+Coppyright 2022 All Rights Reserved.
+Implementation of the Scheduler.
+"""
+
+
 class Task:
     def __init__(self, task, task_id):
+        """
+        The constructor for Task class.
+
+        Parameters:
+        task: The coroutine to be executed.
+        task_id: The ID of task.
+        """
+
         self.id = task_id
         self.task = task
 
@@ -9,13 +25,25 @@ class Task:
 
 class Scheduler:
     def __init__(self):
+        """The constructor for Scheduler class."""
+
         self.tasks = []
 
     def add_task(self, task, task_id):
+        """
+        The function to create new Task object and add to list.
+
+        Parameter:
+        task: The coroutine to be executed.
+        task_id: The ID of task.
+        """
+
         new_task = Task(task, task_id)
         self.tasks.append(new_task)
 
     def schedule(self):
+        """The function to schedule tasks in list sorted by ID."""
+
         self.tasks.sort(key=sort_id)
         while self.tasks:
             task = self.tasks.pop(0)
@@ -27,22 +55,35 @@ class Scheduler:
 
 
 def sort_id(task):
+    """
+    The simple function to return id of Task.
+
+    Parameter:
+    task: The Task object.
+    """
+
     return task.id
 
 
 def program1():
+    """The simple function to print message in for loop."""
+
     for i in range(3):
         print(f'1. program - {i}')
         yield
 
 
 def program2():
+    """The simple function to print message in for loop."""
+
     for i in range(2):
         print(f'2. program - {i}')
         yield
 
 
 def program3():
+    """The simple function to print message in for loop."""
+
     for i in range(4):
         print(f'3. program - {i}')
         yield
