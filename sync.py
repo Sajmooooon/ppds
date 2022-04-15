c3a4ce55fe2ad81d8bf485abfafeb32c742588a4
simon <sajmoooonsk@gmail.com>
2022-04-15 18:11:49 +0200

    docs: add docstrings for sync version

--- sync.py
+++ sync.py
@@ -1,8 +1,23 @@
+"""
+Authors: Bc. Simon Youssef
+         Mgr. Ing. Matúš Jókay, PhD.
+Coppyright 2022 All Rights Reserved.
+Implementation of the simple sync ping function.
+"""
+
+
 import os
 import datetime
 
 
 def ping(url):
+    """
+    Function to ping given url and print if was found or not.
+
+    Parameter:
+        url: URL link of website.
+    """
+
     response = os.system(f"ping {url}")
     if response == 0:
         print(f"{url} was found!\n")
@@ -11,6 +26,8 @@ def ping(url):
 
 
 def main():
+    """This function is for program initialization."""
+
     urls = ["google.com", "facebook.com", "youtube.com", "meet.google.com"]
 
     start = datetime.datetime.now()
