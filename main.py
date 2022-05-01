0216f5773f1552130dae57b1b955dff5582b72aa
simon <sajmoooonsk@gmail.com>
2022-05-01 12:39:30 +0200

    docs: add docstrings

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+"""
+Authors: Bc. Simon Youssef
+         Mgr. Ing. Matúš Jókay, PhD.
+Coppyright 2022 All Rights Reserved.
+Resources:
+https://www.delftstack.com/es/howto/python/convert-image-to-grayscale-python/
+https://www.scratchapixel.com/lessons/digital-imaging/simple-image-manipulations/image-processing
+Implementation of adding yellow filter and brightness to images with streams.
+"""
+
 from matplotlib import image
 from matplotlib import pyplot
 from numba import cuda
@@ -7,6 +17,13 @@ import math
 
 @cuda.jit
 def yellow_filter(io_array):
+    """
+    Simple function to add yellow filter to image.
+
+    Parameter:
+        io_array: Array of image.
+    """
+
     x, y = cuda.grid(2)
     x_max = io_array.shape[0]
     y_max = io_array.shape[1]
@@ -17,6 +34,13 @@ def yellow_filter(io_array):
 
 @cuda.jit
 def gamma(io_array):
+    """
+    Simple function to increase brightness of image.
+
+    Parameter:
+        io_array: Array of image.
+    """
+
     x, y = cuda.grid(2)
     x_max = io_array.shape[0]
     y_max = io_array.shape[1]
@@ -28,6 +52,13 @@ def gamma(io_array):
 
 
 def show_image(image_arr):
+    """
+    Function for showing image.
+
+    Parameter:
+        image_arr: Array of image.
+    """
+
     pyplot.figure()
     figure = pyplot.gcf()
     figure.subplots_adjust(bottom=0, top=1, left=0, right=1)
@@ -43,6 +74,14 @@ def show_image(image_arr):
 
 
 def get_blockspergrid(image_arr, threadsperblock):
+    """
+    Function to get blocks per grid.
+
+    Parameter:
+        image_arr: Array of image.
+        threadsperblock: Tuple of threads per block.
+    """
+
     blockspergrid_x = math.ceil(image_arr.shape[0] / threadsperblock[0])
     blockspergrid_y = math.ceil(image_arr.shape[1] / threadsperblock[1])
     blockspergrid = (blockspergrid_x, blockspergrid_y)
@@ -50,6 +89,15 @@ def get_blockspergrid(image_arr, threadsperblock):
 
 
 def init(arr_len, file_name, type):
+    """
+    Function to get blocks per grid.
+
+    Parameter:
+        arr_len: Length of array.
+        file_name: Name of file.
+        type: Type of function to use (0 - yellow_filter, else - gamma).
+    """
+
     data_gpu = []
     gpu_out = []
     streams = []
